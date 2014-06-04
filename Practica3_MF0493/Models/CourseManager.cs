67f4fd038f70a0e9aa8a6dd9325cd6d2b51b99c6
berna902 <berna85als@gmail.com>
2014-06-04 15:02:04 +0200

    con clase CourseManager

---
+++ Practica3_MF0493/Models/CourseManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica3_MF0493.Models
+{
+    public class CourseManager
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public int DepartmentID { get; set; }
+
+        public CourseManager()
+        {
+            this.Title = "curso";
+            this.Credits = 1;
+        }
+    }
+}
