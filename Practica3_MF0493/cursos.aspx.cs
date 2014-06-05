bbb0a3f9841e51e484045fcf007b2e64d3d2ad61
juanilloforever <juanilloforever@gmail.com>
2014-06-05 11:38:42 +0200

    cambio cursos

--- Practica3_MF0493/cursos.aspx.cs
+++ Practica3_MF0493/cursos.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Practica3_MF0493.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,15 @@ namespace Practica3_MF0493
     public partial class cursos : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+            this.GridView1.DataSource = CourseManager.getAll();
+            this.GridView1.DataBind();
+            }
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
 
         }
