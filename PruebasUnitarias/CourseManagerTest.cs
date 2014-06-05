7734fef3a0d631ea21d8700282495b1882898ee3
berna902 <berna85als@gmail.com>
2014-06-05 13:56:34 +0200

    test cursos

--- PruebasUnitarias/CourseManagerTest.cs
+++ PruebasUnitarias/CourseManagerTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practica3_MF0493.Models;
+using Practica3_MF0493;
+using System.Collections.Generic;
 
 namespace PruebasUnitarias
 {
@@ -9,7 +12,39 @@ namespace PruebasUnitarias
         [TestMethod]
         public void getAllTest()
         {
+            List<Course> lista = CourseManager.getAll();
+            Assert.AreEqual(lista.Count, 10);
+        }
+
+        [TestMethod]
+        public void getTest()
+        {
+            Course c = CourseManager.get(1045);
+            Assert.AreEqual(c.Title, "Calculus");
 
         }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            Course c = new Course();
+            c.CourseID = 1045;
+            c.Title = "berna";
+            c.Credits = 4;
+            c.DepartmentID = 7;
+            int id = CourseManager.Add(c);
+            Assert.AreEqual(id, -1);
+
+
+            c = new Course();
+            c.CourseID = 4062;
+            c.Title = "berna";
+            c.Credits = 4;
+            c.DepartmentID = 7;
+            id = CourseManager.Add(c);
+            Assert.AreNotEqual(id,-1);
+        }
+
+        
     }
 }
