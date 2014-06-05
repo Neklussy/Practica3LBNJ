95a3fd1fccce7d22e09f150abd0e0d5abdcd3f03
Neklussy <lusalane@hotmail.com>
2014-06-05 10:37:28 +0200

    modificado otra ver personamanager

--- Practica3_MF0493/Models/PersonManager.cs
+++ Practica3_MF0493/Models/PersonManager.cs
@@ -12,19 +12,25 @@ namespace Practica3_MF0493.Models
         {
             try
             {
-
+                List<Person> lstPersonas = new List<Person>();
                 using (SchoolEntities db = new SchoolEntities())
                 {
+                    var consulta = from e in db.Person
+                                   select e;
+
+                    per = new Person();
+
+                    
                 }
             }
-            catch (SqlException ex)
+            catch (SqlException sqlex)
             {
-                
+                throw sqlex;
             }
             catch (Exception ex)
             {
-                
 
+                throw ex;
             }
         }
         public Person get(int ID);
