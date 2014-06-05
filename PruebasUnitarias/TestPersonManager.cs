3a6733a88c127345945ae964d242b5c0c804b68e
Neklussy <lusalane@hotmail.com>
2014-06-05 11:28:15 +0200

    Creadas pruebas unitarias

---
+++ PruebasUnitarias/TestPersonManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class TestPersonManager
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
