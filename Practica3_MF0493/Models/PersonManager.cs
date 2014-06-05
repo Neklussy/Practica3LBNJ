e68c27a6826f3b86d8d9687ee07b4986caa612d0
jucles <joseantonioucles@gmail.com>
2014-06-05 09:30:45 +0200

    clases

---
+++ Practica3_MF0493/Models/PersonManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica3_MF0493.Models
+{
+    public class PersonManager: IPersonManager
+    {
+    }
+}
