0e4b8fb4b79ca87c2f9100c7acf10f9edf85eea2
Neklussy <lusalane@hotmail.com>
2014-06-05 10:50:10 +0200

    Dos metodos creados en PersonaManager

--- Practica3_MF0493/Models/PersonManager.cs
+++ Practica3_MF0493/Models/PersonManager.cs
@@ -8,19 +8,28 @@ namespace Practica3_MF0493.Models
 {
     public class PersonManager: IPersonaManager
     {
-        public List<Person> getAll() 
+        public static List<Person> getAll() 
         {
+            List<Person> lstPersonas = new List<Person>();
             try
             {
-                List<Person> lstPersonas = new List<Person>();
                 using (SchoolEntities db = new SchoolEntities())
                 {
                     var consulta = from e in db.Person
                                    select e;
 
-                    per = new Person();
-
-                    
+                    Person per;
+                    foreach (Person p in consulta)
+                    {
+                        per = new Person();
+                        per.PersonID = p.PersonID;
+                        per.FirstName = p.FirstName;
+                        per.LastName = p.LastName;
+                        per.HireDate = p.HireDate;
+                        per.EnrollmentDate = p.EnrollmentDate;
+                        lstPersonas.Add(per);
+                    }
+      
                 }
             }
             catch (SqlException sqlex)
@@ -32,8 +41,32 @@ namespace Practica3_MF0493.Models
 
                 throw ex;
             }
+            return lstPersonas;
         }
-        public Person get(int ID);
+        public Person get(int ID) {
+            Person per = new Person();
+            try
+            {
+                using (SchoolEntities db = new SchoolEntities())
+                {
+                    var consulta = from e in db.Person
+                                   where e.PersonID == ID
+                                   select e;
+
+                    per = consulta.First();
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                throw sqlex;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return per;
+        }
         public int add(Person p);
         public int remove(int iD);
     }
